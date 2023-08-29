91c1f0e9bb0580c9891cb25d5714b972c92d9b12
DGWA_Pham Van Tuan_BE/トァン <tuan.pv@dgwasia.com>
2023-08-29 11:58:16 +0700

    Init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body class="list" id="top">
+    <script>localStorage.getItem("pref-theme") === "dark" && document.body.classList.add("dark")</script>
+    <header class="header">
+        <nav class="nav">
+            <div class="logo flex">
+                <a href="https://rednafi.com/" accesskey="h" title="Redowan's Reflections (Alt + H)" class="flex-1">
+                    <img
+                        src="https://user-images.githubusercontent.com/30027932/234704858-2cff14b0-bdf1-4393-a184-cff35eb9de81.png"
+                        alt aria-label="logo" height="35">
+                    Redowan's Reflections
+                </a>
+                <div class="logo-switches flex-1">
+                    <button id="theme-toggle" accesskey="t" title="(Alt + T)">
+                        <svg id="moon"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none"
+                            stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+                        </svg>
+                        <svg id="sun" xmlns="http://www.w3.org/2000/svg" width="24" height="18"
+                            viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <circle cx="12" cy="12" r="5" />
+                            <line x1="12" y1="1" x2="12" y2="3" />
+                            <line x1="12" y1="21" x2="12" y2="23" />
+                            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                            <line x1="1" y1="12" x2="3" y2="12" />
+                            <line x1="21" y1="12" x2="23" y2="12" />
+                            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <ul id="menu">
+                <li><a href="https://rednafi.com/zephyr/" title="zephyr"><span>zephyr</span></a></li>
+                <li><a href="https://rednafi.com/archives/" title="archives"><span>archives</span></a></li>
+                <li><a href="https://rednafi.com/about/" title="about"><span>about</span></a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main">
+        <article class="first-entry home-info">
+            <header class="entry-header">
+                <h1></h1>
+            </header>
+            <div class="entry-content">
+                <div><i>Hi, I'm Redowan Delowar—a wandering autodidact with a flair for ones and zeros</i>
+                    <div class="bio-urls"><a href="tags">tags</a>
+                        <span>•</span>
+                        <a href="search">search</a>
+                        <span>•</span>
+                        <a href="index.xml">rss</a>
+                    </div>
+                </div>
+            </div>
+            <footer class="entry-footer">
+                <div class="social-icons"></div>
+            </footer>
+        </article>
+        <article class="post-entry">
+            <header class="entry-header">
+                <h2>Escaping the template pattern hellscape in Python</h2>
+            </header>
+            <div class="entry-content">
+                <p>Over the years, I’ve used the template pattern across multiple OO languages with varying degrees of
+                    success. It was one of the first patterns I learned in the primordial hours of my software
+                    engineering career, and for some reason, it just feels like the natural way to tackle many
+                    real-world code-sharing problems. Yet, even before I jumped on board with the composition over
+                    inheritance camp, I couldn’t help but notice how using this particular inheritance technique spawns
+                    all sorts of design and maintenance headaches as the codebase starts to grow....</p>
+            </div>
+            <footer class="entry-footer"><span title='2023-07-01 00:00:00 +0000 UTC'>July 1, 2023</span></footer><a
+                class="entry-link" aria-label="post link to Escaping the template pattern hellscape in Python"
+                href="https://rednafi.com/python/escape_template_pattern" /></a>
+        </article>
+        <article class="post-entry">
+            <header class="entry-header">
+                <h2>Limit goroutines with buffered channels</h2>
+            </header>
+            <div class="entry-content">
+                <p>I was cobbling together a long-running Go script to send webhook messages to a system when some
+                    events occur. The initial script would continuously poll a Kafka topic for events and spawn new
+                    goroutines to make HTTP requests to the destination. This had two problems:
+                    It could create unlimited goroutines if many events arrived quickly It might overload the
+                    destination system by making many concurrent requests In Python, I’d use just asyncio....</p>
+            </div>
+            <footer class="entry-footer"><span title='2023-08-23 00:00:00 +0000 UTC'>August 23, 2023</span></footer><a
+                class="entry-link" aria-label="post link to Limit goroutines with buffered channels"
+                href="https://rednafi.com/go/limit_goroutines_with_buffered_channels" /></a>
+        </article>
+
+
+        <footer class="page-footer">
+            <nav class="pagination"><a class="next" href="https://rednafi.com/page/2" />Next&nbsp;&nbsp;»</a></nav>
+        </footer>
+    </main>
+    <a href="#top" aria-label="go to top" title="Go to Top (Alt + G)" class="top-link" id="top-link" accesskey="g">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6" fill="currentcolor">
+            <path d="M12 6H0l6-6z" />
+        </svg></a>
+
+    <script>document.getElementById("theme-toggle").addEventListener("click", () => { document.body.className.includes("dark") ? (document.body.classList.remove("dark"), localStorage.setItem("pref-theme", "light")) : (document.body.classList.add("dark"), localStorage.setItem("pref-theme", "dark")) })</script>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang=en dir=auto>
+
+<head>
+    <meta name=generator content="Hugo 0.117.0">
+    <meta charset=utf-8>
+    <meta http-equiv=x-ua-compatible content="IE=edge">
+    <meta name=viewport content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name=robots content="index, follow">
+    <title>Redowan's Reflections</title>
+    <meta name=keywords
+        content="Redowan,Delowar,Redowan Delowar,rednafi,SWE,software,software engineering,Redowan Nafi,Nafi,Python,JavaScript,TypeScript,Go,Golang,HTML,CSS,Backend,Networking,APIs,AWS,Google Cloud,Distributed systems,microservices,monoliths,Databases,SQL,SQLite,PostgreSQL,MySQL,NoSQL,Redis,MongoDB,DynamoDB,Django,Flask,FastAPI,Starlette,HTMX,VueJS">
+    <meta name=description content="Ohe, I'm Redowan Delowar—a professional sciolist with a flair for ones and zeros. I sporadically rant and write about Python, JavaScript, Go, SQL, and the broader landscape of software in general.
+    ">
+    <meta name=author content="Redowan Delowar">
+    <link rel=canonical href=https://rednafi.com />
+    <meta name=google-site-verification content="google-site-verification=GoibEK52o5Z7xpKQ7ppTd8bA_s1wQp5hcG7aIWPJJfk">
+    <meta name=yandex-verification content="cb037a09f985e313">
+    <meta name=msvalidate.01 content="XYZabc">
+    <link crossorigin=anonymous
+        href=/assets/css/stylesheet.6292cdb2c62f053f5ae189ee6731f830067c22def87c6b0adcbc88b4844f64d4.css
+        integrity="sha256-YpLNssYvBT9a4YnuZzH4MAZ8It74fGsK3LyItIRPZNQ=" rel="preload stylesheet" as=style>
+    <link rel=icon
+        href=https://user-images.githubusercontent.com/30027932/234704858-2cff14b0-bdf1-4393-a184-cff35eb9de81.png>
+    <link rel=icon type=image/png sizes=16x16
+        href=https://user-images.githubusercontent.com/30027932/234704858-2cff14b0-bdf1-4393-a184-cff35eb9de81.png>
+    <link rel=icon type=image/png sizes=32x32
+        href=https://user-images.githubusercontent.com/30027932/234704858-2cff14b0-bdf1-4393-a184-cff35eb9de81.png>
+    <link rel=apple-touch-icon
+        href=https://user-images.githubusercontent.com/30027932/234704858-2cff14b0-bdf1-4393-a184-cff35eb9de81.png>
+    <link rel=mask-icon
+        href=https://user-images.githubusercontent.com/30027932/234704858-2cff14b0-bdf1-4393-a184-cff35eb9de81.png>
+    <meta name=theme-color content="#2e2e33">
+    <meta name=msapplication-TileColor content="#2e2e33">
+    <link rel=alternate type=application/rss+xml href=https://rednafi.com/index.xml>
+    <link rel=alternate type=application/json href=https://rednafi.com/index.json><noscript>
+        <style>
+            #theme-toggle,
+            .top-link {
+                display: none
+            }
+        </style>
+    </noscript>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-11NK905JK8"></script>
+    <script>var doNotTrack = !1; if (!doNotTrack) { window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments) } gtag("js", new Date), gtag("config", "G-11NK905JK8", { anonymize_ip: !1 }) }</script>
+    <meta property="og:title" content="Redowan's Reflections">
+    <meta property="og:description" content="Ohe, I'm Redowan Delowar—a professional sciolist with a flair for ones and zeros. I sporadically rant and write about Python, JavaScript, Go, SQL, and the broader landscape of software in general.
+    ">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://rednafi.com/">
+    <meta property="og:image"
+        content="https://user-images.githubusercontent.com/30027932/234678636-4748b670-faa3-481a-b74a-abf49b6de261.png">
+    <meta property="og:site_name" content="Redowan's Reflections">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image"
+        content="https://user-images.githubusercontent.com/30027932/234678636-4748b670-faa3-481a-b74a-abf49b6de261.png">
+    <meta name="twitter:title" content="Redowan's Reflections">
+    <meta name="twitter:description"
+        content="Ohe, I'm Redowan Delowar—a professional sciolist with a flair for ones and zeros. I sporadically rant and write about Python, JavaScript, Go, SQL, and the broader landscape of software in general.">
+
+</head>
+
+<body class="list" id="top">
+    <script>localStorage.getItem("pref-theme") === "dark" && document.body.classList.add("dark")</script>
+    <header class="header">
+        <nav class="nav">
+            <div class="logo"><a href="https://rednafi.com/" accesskey="h" title="Redowan's Reflections (Alt + H)"><img
+                        src="https://user-images.githubusercontent.com/30027932/234704858-2cff14b0-bdf1-4393-a184-cff35eb9de81.png"
+                        alt aria-label="logo" height="35">Redowan's Reflections</a>
+                <div class="logo-switches"><button id="theme-toggle" accesskey="t" title="(Alt + T)"><svg id="moon"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none"
+                            stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+                        </svg><svg id="sun" xmlns="http://www.w3.org/2000/svg" width="24" height="18"
+                            viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <circle cx="12" cy="12" r="5" />
+                            <line x1="12" y1="1" x2="12" y2="3" />
+                            <line x1="12" y1="21" x2="12" y2="23" />
+                            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                            <line x1="1" y1="12" x2="3" y2="12" />
+                            <line x1="21" y1="12" x2="23" y2="12" />
+                            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+                        </svg></button></div>
+            </div>
+            <ul id="menu">
+                <li><a href="https://rednafi.com/zephyr/" title="zephyr"><span>zephyr</span></a></li>
+                <li><a href="https://rednafi.com/archives/" title="archives"><span>archives</span></a></li>
+                <li><a href="https://rednafi.com/about/" title="about"><span>about</span></a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main">
+        <article class="first-entry home-info">
+            <header class="entry-header">
+                <h1></h1>
+            </header>
+            <div class="entry-content">
+                <div><i>Hi, I'm Redowan Delowar—a wandering autodidact with a flair for ones and zeros</i>
+                    <div class="bio-urls"><a href="tags">tags</a>
+                        <span>•</span>
+                        <a href="search">search</a>
+                        <span>•</span>
+                        <a href="index.xml">rss</a>
+                    </div>
+                </div>
+            </div>
+            <footer class="entry-footer">
+                <div class="social-icons"></div>
+            </footer>
+        </article>
+        <article class="post-entry">
+            <header class="entry-header">
+                <h2>Escaping the template pattern hellscape in Python</h2>
+            </header>
+            <div class="entry-content">
+                <p>Over the years, I’ve used the template pattern across multiple OO languages with varying degrees of
+                    success. It was one of the first patterns I learned in the primordial hours of my software
+                    engineering career, and for some reason, it just feels like the natural way to tackle many
+                    real-world code-sharing problems. Yet, even before I jumped on board with the composition over
+                    inheritance camp, I couldn’t help but notice how using this particular inheritance technique spawns
+                    all sorts of design and maintenance headaches as the codebase starts to grow....</p>
+            </div>
+            <footer class="entry-footer"><span title='2023-07-01 00:00:00 +0000 UTC'>July 1, 2023</span></footer><a
+                class="entry-link" aria-label="post link to Escaping the template pattern hellscape in Python"
+                href="https://rednafi.com/python/escape_template_pattern" /></a>
+        </article>
+        <article class="post-entry">
+            <header class="entry-header">
+                <h2>Limit goroutines with buffered channels</h2>
+            </header>
+            <div class="entry-content">
+                <p>I was cobbling together a long-running Go script to send webhook messages to a system when some
+                    events occur. The initial script would continuously poll a Kafka topic for events and spawn new
+                    goroutines to make HTTP requests to the destination. This had two problems:
+                    It could create unlimited goroutines if many events arrived quickly It might overload the
+                    destination system by making many concurrent requests In Python, I’d use just asyncio....</p>
+            </div>
+            <footer class="entry-footer"><span title='2023-08-23 00:00:00 +0000 UTC'>August 23, 2023</span></footer><a
+                class="entry-link" aria-label="post link to Limit goroutines with buffered channels"
+                href="https://rednafi.com/go/limit_goroutines_with_buffered_channels" /></a>
+        </article>
+
+
+
+
+
+        <footer class="page-footer">
+            <nav class="pagination"><a class="next" href="https://rednafi.com/page/2" />Next&nbsp;&nbsp;»</a></nav>
+        </footer>
+    </main><a href="#top" aria-label="go to top" title="Go to Top (Alt + G)" class="top-link" id="top-link"
+        accesskey="g"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6" fill="currentcolor">
+            <path d="M12 6H0l6-6z" />
+        </svg></a>
+
+    <script>document.getElementById("theme-toggle").addEventListener("click", () => { document.body.className.includes("dark") ? (document.body.classList.remove("dark"), localStorage.setItem("pref-theme", "light")) : (document.body.classList.add("dark"), localStorage.setItem("pref-theme", "dark")) })</script>
+</body>
+
+</html>
